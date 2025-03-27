6645eaf31f21a2500352f0b0ab040a0af1ee2dde
Kenneth-Arias <alanarias2002@yahoo.com>
2025-03-26 20:51:07 -0600

    Proyecto en proceso Clase 10

--- analisis_datos/estadisticas.py
+++ analisis_datos/estadisticas.py
@@ -1,1 +1,25 @@
 # estadisticas.py
+def media(datos):
+    media_aritmetica = sum(datos) / 5
+    return media_aritmetica
+
+def mediana(datos):
+    #Mediana
+
+    datos = sorted(datos)
+
+    #Cantidad
+
+    n = len(datos)
+
+    mitad = n // 2
+
+#   0     1     2     3
+#(1000, 2000, 3000, 4000)
+
+    if n % 2 == 0:
+        mediana = (datos[mitad -1] + datos [mitad]) / 2 
+    else:
+        mediana = datos[mitad]
+
+    print("La mediana de la compra es de: ", mediana)
--- main.py
+++ main.py
@@ -5,3 +5,18 @@
 # Descripción: Este archivo contiene el punto de entrada principal del proyecto.
 # Recuerda incluir tu nombre completo, la fecha en la que iniciaste el proyecto y la fecha estimada de entrega.
 # Esto ayuda a mantener un registro claro del trabajo realizado.
+
+from analisis_datos import *
+
+lista_compra = generar_lista_compras()
+
+#Antes del for se analiza lo que se quiere a la hora de analizar diccionarios o tuplas
+precios = [precio for producto, precio in lista_compra]
+
+print(precios)
+
+print(lista_compra)
+
+print(media)
+
+print(mediana)
